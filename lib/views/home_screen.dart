849ca0b02cae68864f530e4d1ff30807b00ef2f6
Alan Perez <alanperezse@gmail.com>
2022-11-08 18:30:22 -0700

    Defined view for home screen

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,21 +1,82 @@
+import 'package:cupertino_list_tile/cupertino_list_tile.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
+  Widget _entryCardDate() {
+    return Container(
+      height: 60,
+      width: 60,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(10))
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('FRI', style: const TextStyle(fontWeight: FontWeight.w900),),
+          Text('09/30')
+        ],
+      ),
+    );
+  }
+
+  CupertinoListTile _entryCard() {
+    return CupertinoListTile(
+      leading: _entryCardDate(),
+      title: Flexible(child: Text('Some very long string that would not fit in a line', overflow: TextOverflow.ellipsis)),
+      subtitle: const Text(
+        '2:44PM • 3100 Sea Breeze • Cloudy',
+        style: TextStyle(color: Colors.grey),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      child: CustomScrollView(
-        slivers: [
-          const CupertinoSliverNavigationBar(
-            largeTitle: Text('Home'),
-          ),
-          SliverFillRemaining(
-            child: const Text('Hello World')
-          )
-        ],
-      )
+      navigationBar: CupertinoNavigationBar(
+        middle: const Text('Journal'),
+      ),
+      child: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: CupertinoScrollbar(
+                child: ListView.builder(
+                  itemCount: 100,
+                  itemBuilder: (BuildContext context, int index) {
+                    return _entryCard();
+                  }
+                )
+              )
+            ),
+            Container(
+              decoration: const BoxDecoration(
+                border: Border(
+                  top: BorderSide(color: Color(0xFFBCBBC1), width: 0)
+                )
+              ),
+              child: SizedBox(
+                height: 44,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    const Spacer(),
+                    CupertinoButton(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      child: const Text('Add'),
+                      onPressed: () {},
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
